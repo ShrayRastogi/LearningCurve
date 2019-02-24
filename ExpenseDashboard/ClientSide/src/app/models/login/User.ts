5cd5a6a382793b904fce8e14ee861581bcbfdea2
ShrayRastogi <shray45@gmail.com>
2019-02-24 17:34:42 +0530

    creates a flow to verify user login

--- ExpenseDashboard/ClientSide/src/app/models/login/User.ts
+++ ExpenseDashboard/ClientSide/src/app/models/login/User.ts
@@ -3,4 +3,7 @@ import { FormControl } from '@angular/forms';
 export class User {
   username: FormControl;
   password: FormControl;
+  name: string;
+  id: number;
+  age: number;
 }
--- ExpenseDashboard/ClientSide/src/app/modules/login/service/login.service.ts
+++ ExpenseDashboard/ClientSide/src/app/modules/login/service/login.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/models/login/User';
 
 @Injectable()
 export class LoginService {
   constructor() {}
-  getUsers() {}
+  async getUser(user: User): Promise<User> {
+    return new Promise<User>(resolve => {
+      const validUser = new User();
+      if (user.username.value.toString() === 'shray45') {
+        validUser.name = 'Shray Rastogi';
+        validUser.id = 1;
+        validUser.age = 24;
+        validUser.username = user.username;
+        validUser.password = user.password;
+      }
+      resolve(validUser);
+    });
+  }
 }
