1ab325627e4f3eb53dd42b35f6f0c537944c2ccb
ShrayRastogi <shray45@gmail.com>
2019-07-19 20:23:28 +0530

    A Fresh Start!

--- AngularAnd.NetCore-Demo/ClientSide/src/app/models/login/User.ts
+++
@@ -1,9 +0,0 @@
-import { FormControl } from '@angular/forms';
-
-export class User {
-  username: FormControl;
-  password: FormControl;
-  name: string;
-  id: number;
-  age: number;
-}
--- AngularAnd.NetCore-Demo/ClientSide/src/app/modules/login/login.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { LoginService } from './service/login.service';
-
-@NgModule({
-  declarations: [],
-  imports: [],
-  providers: [LoginService]
-})
-export class LoginModule {}
