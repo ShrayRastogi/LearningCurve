db9353617130a83cf71534307e2fae7801718bcf
ShrayRastogi <shray45@gmail.com>
2019-07-19 20:23:28 +0530

    A Fresh Start!

--- AngularAnd.NetCore-Demo/ClientSide/src/app/customValidators/password.validator.ts
+++
@@ -1,9 +0,0 @@
-import { FormControl } from '@angular/forms';
-
-export function passwordValidator(control: FormControl) {
-  if (control.value && control.value.toString().indexOf('@') < 0) {
-    return { requiredCharacters: true };
-  }
-
-  return null;
-}
--- AngularAnd.NetCore-Demo/ClientSide/src/app/modules/home/component/header/header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.style.scss']
-})
-export class HeaderComponent {}
